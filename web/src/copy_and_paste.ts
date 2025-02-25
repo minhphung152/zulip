cbe39cb7d5d9fa50b68126f3831f75cfea55159e
minhphung152 <98346569+minhphung152@users.noreply.github.com>
2025-02-24 22:25:07 -0500

    copy_paste: Fix issue when copy-pasting from Mac terminal When using Chrome on Mac, copy-pasting from terminal into the compose box creates extra line breaks and spaces. This commit attempts to fix it by adding a new function to check whether the clipboard contains text copied from the Mac terminal. Fixes #33569

--- web/src/copy_and_paste.ts
+++ web/src/copy_and_paste.ts
@@ -385,6 +385,21 @@ export function is_white_space_pre(paste_html: string): boolean {
     );
 }
 
+function is_from_mac_terminal(paste_html: string): boolean {
+    const html_fragment = new DOMParser()
+        .parseFromString(paste_html, "text/html");
+
+    const has_mac_terminal_metadata = [...html_fragment.querySelectorAll("meta")].some(
+        (meta) => (meta.name === "Generator" && meta.content?.includes("Cocoa HTML Writer")),
+    );
+
+    if (!has_mac_terminal_metadata) {
+        return false;
+    }
+    
+    return true;
+}
+
 function is_from_excel(html_fragment: HTMLBodyElement): boolean {
     const html_tag = html_fragment.parentElement;
     if (!html_tag || html_tag.nodeName !== "HTML") {
@@ -924,7 +939,8 @@ export function paste_handler(this: HTMLTextAreaElement, event: JQuery.Triggered
         if (
             !compose_ui.cursor_inside_code_block($textarea) &&
             paste_html &&
-            !compose_ui.shift_pressed
+            !compose_ui.shift_pressed &&
+            !is_from_mac_terminal(paste_html)
         ) {
             if (is_single_image(paste_html)) {
                 // If the copied content is a single image, we let upload.ts handle it.
